b63764e46b2a9160f14f0696e47bb8b487b05ec6
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-08-27 23:44:36 +0600

    Landing page mastering

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,167 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce-17</title>
-</head>
-<body>
-    <h1>Welcome to my home page!!</h1>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Landing Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="{{asset('style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header class="header-section">
+        <div class="hero-content">
+            <h1>Organic Dry Food</h1>
+            <p>Be fit and healthy. Test our organic food</p>
+            <a href="#" class="bt-custom">Buy Now</a>
+        </div>
+    </header>
+
+    <!-- Product Description -->
+    <section class="features-section">
+        <div class="container">
+            <h2>Why Choose Our Product</h2>
+            <div class="row">
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="feature-card">
+                        <img src="{{asset('images/feature.webp')}}">
+                        <h5>Feature-1</h5>
+                        <p>This is a premium product</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Customer Review -->
+     <section class="testimonial-section">
+        <div class="container">
+            <h2>What Our Customer Say</h2>
+            <div class="row">
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4">
+                    <div class="testimonial-content">
+                        <img src="{{asset('images/testimonial.webp')}}">
+                        <p>This is an amazing product</p>
+                        <h5>John Doe</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+
+     <!-- Check Section -->
+      <section class="checkout-section">
+        <div class="container">
+            <h2>Checkout</h2>
+            <form action="" method="" class="checkout-form">
+                <div class="mb-3">
+                    <label for="" class="form-label">Full Name</label>
+                    <input type="text" name="" value="" id="" class="form-control custom-input" required>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Phone Number</label>
+                    <input type="text" name="" value="" id="" class="form-control custom-input" required>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Full Address</label>
+                    <textarea name="" id="" class="form-control custom-input" required></textarea>
+                </div>
+                <button type="submit" class="btn-custom">Place Order</button>
+            </form>
+        </div>
+      </section>
+
+      <!-- Footer Section -->
+       <footer class="footer">
+        <div class="container">
+            <div class="footer-content row">
+                <div class="footer-logo col-12 col-sm-12 col-md-12">
+                    <img src="images/logo.png" height="100" width="180">
+                </div>
+                <div class="social-icons col-12 col-sm-12 col-md-12 text-center">
+                    <a href="#" class="fb"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="twitter"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="#" class="youtube"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+            <p class="text-center mt-3">&copy 2025 Landing Page. All rights reserved</p>
+        </div>
+       </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
 </html>
